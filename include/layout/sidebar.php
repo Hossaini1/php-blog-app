bb2a206bdad52aeb423e1abc8b81c8f39db80735
ali <1989hossaini1@gmail.com>
2025-02-24 15:45:07 +0100

    subscription logik erstellt

--- include/layout/sidebar.php
+++ include/layout/sidebar.php
@@ -31,15 +31,15 @@ $categories = $db->query("SELECT * FROM `categories`");
   <div class="card mt-4">
     <div class="fw-bold fs-6 card-header"> All blogs</div>
     <ul class="list-group list-group-flush p-0">
-    <?php if ($categories->rowCount() > 0): ?>
-      <?php foreach($categories as $category): ?>
-       
+      <?php if ($categories->rowCount() > 0): ?>
+        <?php foreach ($categories as $category): ?>
+
           <li class="list-group-item">
-            <a href="index.php?category=<?= $category['id'] ?>" class="link-body-emphasis text-decoration-none" ><?= $category['title'] ?></a>
+            <a href="index.php?category=<?= $category['id'] ?>" class="link-body-emphasis text-decoration-none"><?= $category['title'] ?></a>
           </li>
-        
-      <?php endforeach ?>
-    <?php endif ?>
+
+        <?php endforeach ?>
+      <?php endif ?>
     </ul>
   </div>
 
@@ -48,17 +48,44 @@ $categories = $db->query("SELECT * FROM `categories`");
     <div class="card-body">
       <p class="fw-bold fs-6">Subscription</p>
 
-      <form>
+      <?php
+
+      $message="";
+
+      if (isset($_POST['subscribe'])) {
+        if (empty(trim($_POST['name']))) {
+          $invalidInputName = 'Name is required!';
+        }elseif(empty(trim($_POST['email']))) {
+          $invalidInputEmail = 'Email is required!';
+        }else{
+          $name = $_POST['name'];
+        $email = $_POST['email'];
+
+        $subscribeInsert = $db->prepare("INSERT INTO `subscribers` (name,email) VALUES(:name, :email) ");
+        $subscribeInsert->execute(['name' => $name, 'email' => $email]);
+
+        $message = "Subscribed successfuly.";
+        }
+       
+
+        
+      }
+      ?>
+
+      <span class='form-text text-success'><?=htmlspecialchars($message)?></span>
+      <form method="POST">
         <div class="mb-3">
           <label class="form-label">Name</label>
-          <input type="text" class="form-control" />
+          <input type="text" class="form-control" name="name" />
+          <?= !empty($invalidInputName) ? "<span class='form-text text-danger'>" . htmlspecialchars($invalidInputName) . "</span>" : ""; ?>
         </div>
         <div class="mb-3">
           <label class="form-label">Email</label>
-          <input type="email" class="form-control" />
+          <input type="email" class="form-control" name="email" />
+          <?= !empty($invalidInputEmail) ? "<span class='form-text text-danger'>" . htmlspecialchars($invalidInputEmail) . "</span>" : ""; ?>
         </div>
         <div class="d-grid gap-2">
-          <button type="submit" class="btn btn-secondary">
+          <button type="submit" class="btn btn-secondary" name="subscribe">
             Submit
           </button>
         </div>
