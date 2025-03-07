60b99dcd8fd997a086e802e7d8f0a0fa9b276078
ali <1989hossaini1@gmail.com>
2025-03-07 11:16:40 +0100

    comments gefetcht und in single page angezeigt

--- single.php
+++ single.php
@@ -1,5 +1,8 @@
  <!-- header -->
  <?php
+
+use Dom\Comment;
+
     include "./include/layout/header.php";
 
 
@@ -17,169 +20,140 @@
      <section class="mt-4">
          <div class="row">
 
-           <!-- Sidebar Section -->
+             <!-- Sidebar Section -->
 
-           <?php
+             <?php
                 include "./include/layout/sidebar.php"
                 ?>
 
              <!-- Posts & Comments Content -->
-              <?php if(!empty($post)) : ?>
-             <div class="col-lg-8">
-                 <div class="row justify-content-center">
-                     <!-- Post Section -->
-                     <div class="col">
-                         <div class="card">
-                             <img
-                                 src="./uploads//posts/<?= $post['image'] ?>"
-                                 class="card-img-top"
-                                 alt="post-image" />
-                             <div class="card-body">
-                                 <div
-                                     class="d-flex justify-content-between">
-                                     <h5 class="card-title fw-bold">
-                                         لورم ایپسوم
-                                     </h5>
+             <?php if (!empty($post)) : ?>
+                 <div class="col-lg-8">
+                     <div class="row justify-content-center">
+                         <!-- Post Section -->
+                         <div class="col">
+                             <div class="card">
+                                 <img
+                                     src="./uploads//posts/<?= $post['image'] ?>"
+                                     class="card-img-top"
+                                     alt="post-image" />
+                                 <div class="card-body">
+                                     <div
+                                         class="d-flex justify-content-between">
+                                         <h5 class="card-title fw-bold">
+                                             لورم ایپسوم
+                                         </h5>
+                                         <div>
+                                             <?php
+                                                $categoryId = $post['category_id'];
+                                                $category = $db->query("SELECT * FROM `categories` WHERE id=$categoryId")->fetch()
+                                                ?>
+                                             <span
+                                                 class="badge bg-primary"><?= $category['title'] ?></span>
+                                         </div>
+                                     </div>
+                                     <p
+                                         class="card-text text-secondary text-justify pt-3"><?= $post['body'] ?> </p>
                                      <div>
-                                        <?php 
-                                        $categoryId = $post['category_id'];
-                                        $category = $db->query("SELECT * FROM `categories` WHERE id=$categoryId")->fetch()
-                                        ?>
-                                         <span
-                                             class="badge bg-primary"><?= $category['title'] ?></span>
+                                         <p class="fs-6 mt-5 mb-0">Author:
+                                             <?= $post['author'] ?>
+                                         </p>
                                      </div>
                                  </div>
-                                 <p
-                                     class="card-text text-secondary text-justify pt-3"><?= $post['body'] ?> </p>
-                                 <div>
-                                     <p class="fs-6 mt-5 mb-0">Author:
-                                         <?= $post['author'] ?>
-                                     </p>
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
-
-                     <hr class="mt-4" />
-
-                     <!-- Comment Section -->
-                     <div class="col">
-                         <!-- Comment Form -->
-                         <div class="card">
-                             <div class="card-body">
-                                 <p class="fw-bold fs-5">
-                                     ارسال کامنت
-                                 </p>
-
-                                 <form>
-                                     <div class="mb-3">
-                                         <label class="form-label">نام</label>
-                                         <input
-                                             type="text"
-                                             class="form-control" />
-                                     </div>
-                                     <div class="mb-3">
-                                         <label class="form-label">متن کامنت</label>
-                                         <textarea
-                                             class="form-control"
-                                             rows="3"></textarea>
-                                     </div>
-                                     <button
-                                         type="submit"
-                                         class="btn btn-dark">
-                                         ارسال
-                                     </button>
-                                 </form>
                              </div>
                          </div>
 
                          <hr class="mt-4" />
-                         <!-- Comment Content -->
-                         <p class="fw-bold fs-6">تعداد کامنت : 3</p>
 
-                         <div class="card bg-light-subtle mb-3">
-                             <div class="card-body">
-                                 <div
-                                     class="d-flex align-items-center">
-                                     <img
-                                         src="./assets/images/profile.png"
-                                         width="45"
-                                         height="45"
-                                         alt="user-profle" />
+                         <!-- Comment Section -->
+                         <div class="col">
+                             <!-- Comment Form -->
+                             <div class="card">
+                                 <div class="card-body">
+                                     <p class="fw-bold fs-5">
+                                         ارسال کامنت
+                                     </p>
 
-                                     <h5
-                                         class="card-title me-2 mb-0">
-                                         محمد صالحی
-                                     </h5>
+                                     <form>
+                                         <div class="mb-3">
+                                             <label class="form-label">نام</label>
+                                             <input
+                                                 type="text"
+                                                 class="form-control" />
+                                         </div>
+                                         <div class="mb-3">
+                                             <label class="form-label">متن کامنت</label>
+                                             <textarea
+                                                 class="form-control"
+                                                 rows="3"></textarea>
+                                         </div>
+                                         <button
+                                             type="submit"
+                                             class="btn btn-dark">
+                                             ارسال
+                                         </button>
+                                     </form>
                                  </div>
-
-                                 <p class="card-text pt-3 pr-3">
-                                     لورم ایپسوم متن ساختگی با تولید
-                                     سادگی نامفهوم از صنعت چاپ و با
-                                     استفاده از طراحان گرافیک است.
-                                 </p>
                              </div>
-                         </div>
 
-                         <div class="card bg-light-subtle mb-3">
-                             <div class="card-body">
-                                 <div
-                                     class="d-flex align-items-center">
-                                     <img
-                                         src="./assets/images/profile.png"
-                                         width="45"
-                                         height="45"
-                                         alt="user-profle" />
+                             <hr class="mt-4" />
+                             <!-- Comment Content -->
+                             <?php
 
-                                     <h5
-                                         class="card-title me-2 mb-0">
-                                         متین سیدی
-                                     </h5>
+                                $postId = $post['id'];
+                                $comments = $db->prepare("SELECT * FROM comments WHERE post_id = :id AND status = '1' ");
+                                $comments->execute(["id" => $postId]);
+
+                              
+                                ?>
+                             <p class="fw-bold fs-6">Comments: <?= $comments->rowCount() ?></p>
+                             <?php if($comments->rowCount()>0): ?> 
+
+                             <?php foreach ($comments as $comment) :?>
+
+                             <div class="card bg-light-subtle mb-3">
+                                 <div class="card-body">
+                                     <div
+                                         class="d-flex align-items-center">
+                                         <img
+                                         
+                                             src="./assets/images/profile.png"
+                                             width="45"
+                                             height="45"
+                                             alt="user-profle" 
+                                             class="card-title me-2 "/>
+
+                                         <h5
+                                             class="card-title mb-0 text-secondary">
+                                               <?= $comment['name']; ?>
+                                         </h5>
+                                     </div>
+
+                                     <p class="card-text pt-3 pr-3">
+                                         <?= $comment['comment']; ?>
+                                     </p>
                                  </div>
-
-                                 <p class="card-text pt-3 pr-3">
-                                     لورم ایپسوم متن ساختگی با تولید
-                                     سادگی نامفهوم از صنعت چاپ
-                                 </p>
                              </div>
-                         </div>
+                             <?php endforeach ?>
+                             <?php else : ?>
+                                <div class="alert alert-danger">There is no comment yet.</div>
+                             <?php endif ?>
 
-                         <div class="card bg-light-subtle mb-3">
-                             <div class="card-body">
-                                 <div
-                                     class="d-flex align-items-center">
-                                     <img
-                                         src="./assets/images/profile.png"
-                                         width="45"
-                                         height="45"
-                                         alt="user-profle" />
 
-                                     <h5
-                                         class="card-title me-2 mb-0">
-                                         زهرا عزیزی
-                                     </h5>
-                                 </div>
-
-                                 <p class="card-text pt-3 pr-3">
-                                     لورم ایپسوم متن ساختگی با تولید
-                                     سادگی
-                                 </p>
-                             </div>
                          </div>
                      </div>
                  </div>
-             </div>
 
              <?php else : ?>
 
-                <div class="col-lg-8">
-                    <div class="alert alert-danger">
-                        There is no post!
-                    </div>
-                </div>
+                 <div class="col-lg-8">
+                     <div class="alert alert-danger">
+                         There is no post!
+                     </div>
+                 </div>
              <?php endif ?>
 
-       
+
          </div>
      </section>
  </main>
