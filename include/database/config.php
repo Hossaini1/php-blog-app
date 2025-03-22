aaaee6e42e2875b669f2ca3c30ecf2bed9145389
Ali <138576472+Hossaini1@users.noreply.github.com>
2025-03-22 11:23:27 +0100

    Delete include/database directory

--- include/database/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-define("DNS","mysql:host=localhost;dbname=php_blog_app;");
-define("DB_USER","root");
-define("DB_PASS","");
-?>
--- include/database/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-// include "config.php";
-
-$db = new PDO(DNS , DB_USER, DB_PASS);
-
-?>
